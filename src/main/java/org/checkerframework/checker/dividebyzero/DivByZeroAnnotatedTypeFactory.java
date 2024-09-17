cd327929eda4cbd99363cb92c6d58795421f67b4
Chirag Gupta <chirag.gupta2992@gmail.com>
2024-09-17 14:56:40 -0400

    Added default annotation assignment

--- src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroAnnotatedTypeFactory.java
+++ src/main/java/org/checkerframework/checker/dividebyzero/DivByZeroAnnotatedTypeFactory.java
@@ -28,12 +28,10 @@ public class DivByZeroAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
     switch (literal.getKind()) {
       case INT_LITERAL:
         int intValue = (Integer) literal.getValue();
-        // TODO
-        break;
+        return intValue < 0 ? Negative.class : intValue > 0 ? Positive.class : Zero.class;
       case LONG_LITERAL:
         long longValue = (Long) literal.getValue();
-        // TODO
-        break;
+        return longValue < 0 ? Negative.class : longValue > 0 ? Positive.class : Zero.class;
     }
     return Top.class;
   }
